a3f89378353584dfb7418f4ad11cf5fa30605a47
analogouscircuit <david.dahlbom@gmail.com>
2020-11-28 16:17:57 -0500

    added support for SAIs

--- src/JCARFAC.jl
+++ src/JCARFAC.jl
@@ -11,7 +11,7 @@ module JCARFAC
 
 using Match
 
-export calcnap, defaultparams, BMParams, IHCParams, OHCParams
+export calcnap, calcsai, defaultparams, BMParams, IHCParams, OHCParams
 
 ################################################################################
 # Data Types
@@ -52,6 +52,15 @@ struct SAI
     images::Ptr{Cdouble} 
     delay_t::Ptr{Cdouble} 
     times::Ptr{Cdouble} 
+end
+
+struct JSAI
+    numframes::Int64
+    numsections::Int64
+    framelen::Int64
+    images::Array{Float64,3}
+    delays::Array{Float64,1}
+    times::Array{Float64,1}
 end
 
 struct CARFACAGCState
@@ -114,17 +123,18 @@ end
 # Utility Functions (for internal use)
 ################################################################################
 """
-    init(bmp::Ref{BMParams}, ihcp::Ref{IHCParams}, ohcp::Ref{OHCParams},
+    initcarfac(bmp::Ref{BMParams}, ihcp::Ref{IHCParams}, ohcp::Ref{OHCParams},
 
 Initialize the CARFAC state structure.
 """
-function init(bmp::Ref{BMParams}, ihcp::Ref{IHCParams}, ohcp::Ref{OHCParams},
+function initcarfac(bmp::Ref{BMParams}, ihcp::Ref{IHCParams}, ohcp::Ref{OHCParams},
               blocksize::Int, fs::Float64)
     blocksize = Int32(blocksize)
     carfacstateptr = ccall(
-                        (:carfacagc_init, "../ccarfac/src/libcarfac.so"),
+                        (:carfacagc_init, "/home/dahlbom/research/carfac/ccarfac/src/libcarfac.so"),
                         Ptr{CARFACAGCState},
-                        (Ref{BMParams}, Ref{IHCParams}, Ref{OHCParams}, Int32, Float64),
+                        (Ref{BMParams}, Ref{IHCParams}, Ref{OHCParams},
+                         Int32, Float64),
                         bmp, ihcp, ohcp, blocksize, fs)
     if carfacstateptr == C_NULL
         error("CARFAC initialization process returned null.")
@@ -132,8 +142,8 @@ function init(bmp::Ref{BMParams}, ihcp::Ref{IHCParams}, ohcp::Ref{OHCParams},
     return carfacstateptr
 end
 
-init(bmp::BMParams, ihcp::IHCParams, ohcp::OHCParams, blocksize::Int, fs::Float64) = 
-    init(Ref(bmp), Ref(ihcp), Ref(ohcp), blocksize, fs)
+initcarfac(bmp::BMParams, ihcp::IHCParams, ohcp::OHCParams, blocksize::Int, fs::Float64) = 
+    initcarfac(Ref(bmp), Ref(ihcp), Ref(ohcp), blocksize, fs)
 
 
 """
@@ -145,7 +155,7 @@ function processblock(carfacstate::Ptr{CARFACAGCState}, signal::Array{Float64,1}
     # @assert carfacstate.num_sections == length(signal) "Block size doesn't match signal length"
     signal_ref = Base.unsafe_convert(Ptr{Cdouble}, Base.cconvert(Ptr{Cdouble}, signal))
     success = ccall(
-                    (:carfacagc_process_block, "../ccarfac/src/libcarfac.so"),
+                    (:carfacagc_process_block, "/home/dahlbom/research/carfac/ccarfac/src/libcarfac.so"),
                     Cint,
                     (Ptr{CARFACAGCState},Ptr{Cdouble}),
                     carfacstate, signal_ref)
@@ -167,14 +177,21 @@ seemed to cause problems when using carfacagc_free_except_signal...
 
 Here we're just copying the data over so the whole structure can be freed at once.)
 """
-function copynap(carfacstate::Ptr{CARFACAGCState})
+function copynap(carfacstate::Ptr{CARFACAGCState}; reshape=true)
     carfacstate_local = Base.unsafe_load(carfacstate)
     numsections = carfacstate_local.num_sections
     numpoints = carfacstate_local.block_size
-    fcs = unsafe_wrap(Array, carfacstate_local.f, numsections, own=false) |> copy
-    nap = unsafe_wrap(Array, carfacstate_local.ihc_out, numsections*numpoints, own=false) |> copy
+    fcs = unsafe_wrap(Array, carfacstate_local.f,
+                      numsections, own=false) |> copy
+    nap = unsafe_wrap(Array, carfacstate_local.ihc_out,
+                      numsections*numpoints, own=false) |> copy
     fcs = reverse(fcs)
-    nap = nap |> x -> reshape(x, (numpoints, numsections)) |> transpose |> x -> reverse(x, dims=1)
+    if reshape
+        nap = nap |>
+              x -> reshape(x, (numpoints, numsections)) |>
+              transpose |>
+              x -> reverse(x, dims=1)
+    end
     # success = ccall(
     #               (:carfacagc_free_except_signal,
     #               "/home/dahlbom/research/ccarfac/libcarfac.so"),
@@ -189,25 +206,76 @@ end
 
 
 """
+    copysai(sai::Ptr{SAI})
+
+Copy SAI data into Julia structure.  (Doesn't take ownership
+of data, since C function is called to free the original data structure.)
+"""
+function copysai(sai::Ptr{SAI})
+    # Copy data locally
+    sai_local = Base.unsafe_load(sai)
+    numframes = Int64(sai_local.num_frames)
+    numsections = Int64(sai_local.num_sections)
+    framelen = Int64(sai_local.frame_len_n)
+    delays = unsafe_wrap(Array, sai_local.delay_t, framelen, own=false) |> copy
+    times = unsafe_wrap(Array, sai_local.times, numframes, own=false) |> copy
+    images = unsafe_wrap(Array, sai_local.images,
+                         numframes*numsections*framelen, own=false) |> copy
+    # Massage data layout
+    offset = numsections*framelen 
+    imagesnew = Array{Float64,3}(undef,numframes,numsections,framelen)
+    for k ∈ 1:numframes
+        imagesnew[k,:,:] = images[(k-1)*offset+1:k*offset] |>
+                       x -> reshape(x, (framelen, numsections)) |>
+                       transpose |>
+                       x -> reverse(x, dims=1)
+    end
+    JSAI(numframes, numsections, framelen, imagesnew, delays, times) 
+end
+
+
+"""
     free(carfacstate::Ptr{CARFACAGCState})
 
 Free the CARFAC state data.
 """
 function free(carfacstate::Ptr{CARFACAGCState})
-    success = ccall(
-          (:carfacagc_free,"../ccarfac/src/libcarfac.so"),
-          Cint,
-          (Ptr{CARFACAGCState},),
-          carfacstate)
+    success = ccall((:carfacagc_free,"/home/dahlbom/research/carfac/ccarfac/src/libcarfac.so"),
+                    Cint,
+                    (Ptr{CARFACAGCState},),
+                    carfacstate)
     if success != 0
         error("Failed to free CARFAC state.")
     end
     return nothing
 end
 
+
+"""
+    free(sai::Ptr{SAI})
+
+Free the SAI data.
+"""
+function free(sai::Ptr{SAI})
+    success = ccall((:sai_free, "/home/dahlbom/research/carfac/ccarfac/src/libcarfac.so"),
+                    Cint,
+                    (Ptr{SAI},),
+                    sai)
+    if success != 0
+        error("Failed to free SAI state.")
+    end
+    return nothing
+end
+
+
 ################################################################################
 # Public Interface
 ################################################################################
+OHCParams() = OHCParams(0.1,0.04,bvals_ref)
+BMParams() = BMParams(numsections,0.1,0.9,0.2)
+IHCParams() = IHCParams(20.0,10.0e-3,0.5e-3,80.0e-6)
+
+
 """
     defaultparams(numsections=72)
 
@@ -233,16 +301,58 @@ Calculate the neural activity pattern (NAP) generated by a signal. This
 is the main interface to the module. Returns a 2D array containing
 the NAP as well as a set of center frequencies corresponding to each channel.
 """
-function calcnap(bmp::BMParams, ihcp::IHCParams, ohcp::OHCParams,
-                signal::Array{Float64,1}, fs::T) where T <: Number
-    state = init(bmp, ihcp, ohcp, length(signal), Float64(fs))
+function calcnap(signal::Array{Float64,1}, fs::T,
+                 bmp::BMParams, ihcp::IHCParams, ohcp::OHCParams) where T <: Number
+    state = initcarfac(bmp, ihcp, ohcp, length(signal), Float64(fs))
     processblock(state, signal)
     nap, fcs = copynap(state)
     free(state)
     return nap, fcs
 end
 
-calcnap(signal::Array{Float64,1}, fs::T) where T <: Number = calcnap(defaultparams()..., signal, fs)
+calcnap(signal::Array{Float64,1}, fs::T) where T <: Number =
+        calcnap(signal, fs, defaultparams()...)
 
+
+"""
+    calcsai(signal::Array{Float64,1}, fs::T,
+            bmp::BMParams, ihcp::IHCParams, ohcp::OHCParams;
+            trigwindowtime = 0.010, advancestep = 0.005,
+            numtrigwindows = 4) where T <: Number
+
+Calculate frames of a movie consisting of stabilized auditory
+images using the CARFAC model of the auditory periphery. Returns
+a three dimensional array, the first index of which corresponds
+to frame number, the second to channel, and the third to sample.
+"""
+function calcsai(signal::Array{Float64,1}, fs::T,
+                 bmp::BMParams, ihcp::IHCParams, ohcp::OHCParams;
+                 trigwindowtime = 0.010, advancestep = 0.005,
+                 numtrigwindows = 4) where T <: Number
+    fs = Float64(fs)
+    state_ptr = initcarfac(bmp, ihcp, ohcp, length(signal), fs)
+    processblock(state_ptr, signal)
+    nap, fcs = copynap(state_ptr; reshape=false)
+    carfacstate_local = Base.unsafe_load(state_ptr)
+    numsections = carfacstate_local.num_sections
+    numpoints = carfacstate_local.block_size
+    saip = SAIParams(trigwindowtime, advancestep, numtrigwindows,
+                     numsections, numpoints)
+    #saip_ref = Base.unsafe_convert(Ptr{SAI}, Base.cconvert(Ptr{SAI}, saip))
+    sai_ptr = ccall((:sai_generate, "/home/dahlbom/research/carfac/ccarfac/src/libcarfac.so"),
+                    Ptr{SAI},
+                    (Ptr{Cdouble}, Cdouble, Ptr{SAIParams}),
+                    nap, fs, Ref(saip))
+    sai = copysai(sai_ptr)
+    free(state_ptr)
+    free(sai_ptr)
+    return sai, fcs
+end
+
+function calcsai(signal::Array{Float64,1}, fs::T;
+                 trigwindowtime = 0.010, advancestep = 0.005,
+                 numtrigwindows = 4) where T <: Number = 
+    calcsai(signal, fs, defaultparams()...; trigwindowtime=trigwindowtime,
+            advancestep=advancestep, numtrigwindows=numtrigwindows)
 
 end
